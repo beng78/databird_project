0299a57aca074d60223d4f8e0aafaaecdb717004
beng78 <benoit.gruet@gmail.com>
2025-01-24 11:48:40 +0100

    commit 3

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,10 +1,10 @@
 select
-    order_id,
-    user_name as user_id,
-    order_status,
-    DATETIME(order_date, "Europe/Paris") AS order_created_atx,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_atx,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_atx,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_atx,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_deliveryx
+    order_id AS order_id,
+    user_name AS user_id,
+    order_status AS order_status,
+    DATETIME(order_date, "Europe/Paris") AS order_created_at,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
 from {{ source('sales_database', 'order') }}
